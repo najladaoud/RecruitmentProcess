197a23e96e8f512aa67aaddb5a5aba8ec3554c18
najladaoud <57408260+najladaoud@users.noreply.github.com>
2021-08-18 16:07:02 +0100

    Initialization project closes #1

---
+++ src/main/java/tn/inetum/RecruitmentProcess/RecruitmentProcessApplication.java
@@ -0,0 +1,13 @@
+package tn.inetum.RecruitmentProcess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecruitmentProcessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecruitmentProcessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tn/inetum/RecruitmentProcess/RecruitmentProcessApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.inetum.RecruitmentProcess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecruitmentProcessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
